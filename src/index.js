e6d8973bcc376cb21d64fb95bc48c85516cf1b63
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-09-16 17:04:48 -0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import './style.css';
-import api from './modules/api.js';
-import { manageData, loadFromApiIngridients/* , meals */ } from './modules/renderMeal.js';
 
-manageData();
+import displayCards from './modules/displayItems.js';
 
-/* search button calling other Meals to the API by name */
-const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', async () => {
-  const data = await api.getByIngredient();
-  if (data) {
-    const x = [...data];
-    const meal = x.slice(0, 9);
-    loadFromApiIngridients(meal);
-    console.log(meal);
-  } else {
-    alert('Sorry, we didn`t find any meal!');
-  }
+const recipes = document.querySelector('#meals');
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayCards();
+  recipes.classList.remove('hidden');
 });
-
-// pop alert code not working!!.
-
-/* meals.innerHTML = '';
-const popError = document.createElement('div');
-popError.classList.add('notFound');
-const noMeal = document.createElement('h2');
-noMeal.textContent = 'Sorry, we didn`t find any meal!';
-popError.appendChild(noMeal);
-meals.appendChild(popError);
-
-const deletePopError = document.querySelector('notFound');
-meals.addEventListener('click', () => {
-  deletePopError.classList.remove('notFound');
-}); */
